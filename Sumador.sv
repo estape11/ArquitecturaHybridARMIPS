1b43efc8bd72f300997df3eaa02d4c098dea743a
michaelGonzalez1812 <michael.gonzalez1812@gmail.com>
2018-09-22 10:56:38 -0600

    modulo sumador y Prueba

--- Sumador.sv
+++ Sumador.sv
@@ -1,6 +1,6 @@
-module(input sumando1,
-		 input sumando2,
-		 output logic resultado);
+module Sumador(input [7:0]sumando1,
+		         input [7:0]sumando2,
+		         output logic [7:0]resultado);
 	
 	always @(sumando1, sumando2) begin
 		resultado = sumando1 + sumando2;
---
+++ SumadorTB.sv
@@ -0,0 +1,20 @@
+module SumadorTB();
+	
+	logic [7:0]sumando1;
+	logic [7:0]sumando2;
+	logic	[7:0]resultado;
+	
+	Sumador DUT(sumando1, sumando2, resultado);
+	
+	initial begin
+		resultado = 0;
+		sumando1 = 1;
+		sumando2 = 1;
+		#2
+		sumando1 = 2;
+		sumando2 = 2;
+		#2
+		sumando1 = 3;
+		sumando2 = 3;
+	end
+endmodule
