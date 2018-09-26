b72524b1515ce0ccaa00fa1a7f1439439e4a0d14
Esteban AP <estape11@gmail.com>
2018-09-26 04:02:11 -0600

    Merge

--- Fetch.sv
+++ Fetch.sv
@@ -1,26 +1,43 @@
-/*WARNING******************************
-Se debe iginorar el primer ciclo
-para evitar que la primera instrucción
-se ejecute dos veces.
-***************************************/
-
-module Fetch(
-	input clk,
-	input PCsrc,
-	input [31:0] PCalu,
-	output [31:0] inst //instrucción
-	);
+/**
+**********************************************************************
+		Instituto Tecnologico de Costa Rica 
+			Ingenieria en Electronica
+						Fetch
+       
+		Autores: Esteban Aguero Perez
+					Michael Gonzalez Rivera
+					Daniela Hernandez Alvarado
+					
+			Lenguaje: SystemVerilog
+					Version: 1.0         
+		Ultima Modificacion: 26/09/2018
 	
-	logic [31:0] pc = 0;
-	logic [31:0] PCsumador;
+	Entradas:- 3 operandos de entrada
+				- clock
+				- Fuente para leer pec
+				- PC proveniente de la ALU
+				
+	Restricciones:
+				- Entradas son de N bits 
+	
+   Salidas: - instrucción leida
+            
+		Arquitectura de Computadores I 2018
+				Prof. Ronald Garcia
+**********************************************************************
+*/
+module Fetch #(parameter N = 32)
+              (input  clk, PCsrc,
+					input  [N-1:0] PCalu,
+					output [N-1:0] inst);
+	
+	logic [N-1:0] pc = 0;
+	logic [N-1:0] PCsumador;
 	
 	Sumador sumador(pc, 1, PCsumador);
 	InstructionMemory memoria_instrucciones(pc, inst);
 	
-	always_ff @(posedge clk) begin
-		if (PCsrc)
-			pc = PCalu;
-		else
-			pc = PCsumador;
-	end
+	always_ff @(posedge clk)
+		pc = PCsrc ? PCalu : PCsumador;
+	
 endmodule
