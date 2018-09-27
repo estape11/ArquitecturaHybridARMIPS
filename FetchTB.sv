4c4f65d5d0c8ebb211068174a71a1967e3870327
michaelGonzalez1812 <michael.gonzalez1812@gmail.com>
2018-09-27 02:35:34 -0600

    Se solucionaron bugs en fetch

--- FetchTB.sv
+++ FetchTB.sv
@@ -13,19 +13,7 @@ module FetchTB();
 	
 	Fetch fetch(clk, PCsrc, PCalu, inst);
 	
-	
-	/*Prueba 1
-		1. llenar 3 posiciones de memoria de instrucciones (Realizado directamente en el simulador)
-		2. Utilizando solamente el sumador leer 4 instrucciones
-	*/
 	/*
-	initial begin
-		PCsrc <= 0; //Acutaliza el PC con el valor del sumador
-		PCalu <= 0; 
-	end
-	*/
-	
-	/*Prueba 2
 		1. Llenar 5 instrucciones
 		2. Leer primeras 2 utilizando el sumador
 		3. Saltar la tercera e ir a la cuarta
@@ -33,14 +21,14 @@ module FetchTB();
 	*/
 	
 	initial begin
-		PCsrc <= 0;
-		PCalu <= 0;
+		PCsrc <= 1'b0;
+		PCalu <= 32'h0;
 		
 		#4
-		PCsrc <= 1;
-		PCalu <= 5;
+		PCsrc <= 1'b1;
+		PCalu <= 32'h5;
 		#2
-		PCsrc <= 0;		
+		PCsrc <= 1'b0;		
 	end
 	
 	
